5fcb9eec4acfc3d492420376b532f3ef3aaf92a4
nanteOS <hrajaona@outsourcia-group.com>
2022-10-17 16:29:59 +0300

    Link Error
    Control Page Not Found with switch

---
+++ src/components/Error/index.jsx
@@ -0,0 +1,9 @@
+import { Link } from 'react-router-dom'
+ 
+export default function Error() {
+    return (
+        <div>
+            <h1>Oops 🙈 This page doesn't exist</h1>
+        </div>
+    )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,19 +3,28 @@ import ReactDOM from "react-dom";
 import Home from "./pages/Home";
 import Survey from "./pages/Survey";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Header from "./components/Header";
+import Error from "./components/Error";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Header />
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/survey/:questionNumber">
-        <Survey />
-      </Route>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/survey/:questionNumber">
+          <Survey />
+        </Route>
+        <Route path="/:questionNumber">
+          <Home />
+        </Route>
+        <Route>
+          <Error />
+        </Route>
+      </Switch>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
