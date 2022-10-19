feb161fefb14675a50c998f834011eebb86321a4
nanteOS <hrajaona@outsourcia-group.com>
2022-10-19 17:23:18 +0300

    State/Props
    Use state in styled components

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,29 @@
+
+import { useState } from "react";
+import styled from "styled-components";
+
+const HomeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+`
+
+const Ballom = styled.div`
+  height: 100px;
+  width: 100px;
+  display: flex;
+  border-radius: 50px;
+  background-color: #e20202;
+  transform: scale(${({size}) => size })
+`
+
 export default function Index() {
-  return <div className="App">Home</div>;
+  const [size, setSize] = useState(1);
+  return (
+    <HomeContainer>
+      <h1 onClick={() => setSize(size + 0.1)}> Home page </h1>
+      <Ballom size={size}/>
+    </HomeContainer>
+  )
 }
